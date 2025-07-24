f527c81ee3fdaa990cf9c0a7904a0e53b8000cc4
Garrett Eckl <geckl@mcneese.edu>
2025-07-24 18:02:55 -0500

    Moving ToneJS code out of child components

--- apps/Conductor/src/components/Backtrack/BacktrackButton.tsx
+++ apps/Conductor/src/components/Backtrack/BacktrackButton.tsx
@@ -1,164 +1,28 @@
-import { React, useRef, useEffect } from 'react';
-import { Button, CloseButton, FileUpload, Input, InputGroup, useFileUpload, VStack } from "@chakra-ui/react"
-import { Socket } from 'socket.io-client';
-import { convertTime, streamFile, toBase64 } from '../../util';
-import * as Tone from "tone";
-import Peaks from 'peaks.js';
+import { React, useEffect } from 'react';
+import { CloseButton, FileUpload, Input, InputGroup, useFileUpload, VStack } from "@chakra-ui/react"
 
 // export const BacktrackButton = ({ backtrack, setBacktrack, socket }: { backtrack: MutableRefObject<Tone.Player>, setBacktrack: (p: Tone.Player) => void , socket: Socket | null }) => {
-export const BacktrackButton = ({ backtrack, socket, togglePlayback }) => {
+export const BacktrackButton = ({ setBacktrack, readBacktrackFile }: { setBacktrack: (b: ArrayBuffer | null) => void; readBacktrackFile: (f: File | null) => void }) => {
     const fileUpload = useFileUpload({
         maxFiles: 1,
         accept: ["audio/mpeg", "audio/wav"],
     });
 
-    const peaks = useRef(null);
-    const updatePlayhead = useRef(null);
-
-
     useEffect(() => {
         const file = fileUpload.acceptedFiles[0];
-        console.log("New Backing Track Selected: ", file);
-
-        if (socket && file) {
-            streamFile({ audioFile: file, socket });
-        }
-
         if (file) {
+            console.log("New Backing Track Selected: ", file);
+            readBacktrackFile(file);
             const backtrackBlob = file as Blob;
             const fileReader = new FileReader();
             fileReader.onloadend = () => {
-
                 const arrayBuffer = fileReader.result as ArrayBuffer
-
-                // Convert array buffer into audio buffer
-                Tone.getContext().decodeAudioData(arrayBuffer).then((audioBuffer) => {
-                    backtrack.current = new Tone.Player(audioBuffer).sync().start(0).toDestination();
-
-                    const player = {
-                        externalPlayer: backtrack.current,
-                        eventEmitter: null,
-
-                        init: function (eventEmitter) {
-                            console.log("init backtrack!");
-                            this.eventEmitter = eventEmitter;
-
-                            // this.externalPlayer.sync();
-                            // this.externalPlayer.start();
-
-                            Tone.getTransport().position = "0:0:0";
-
-                           updatePlayhead.current = Tone.getTransport().scheduleRepeat(() => {
-                                const time = this.getCurrentTime();
-                                eventEmitter.emit('player.timeupdate', time);
-
-                                if (time >= this.getDuration()) {
-                                    Tone.getTransport().stop();
-                                }
-                            }, 0.25);
-
-                            return Promise.resolve();
-                        },
-
-                        destroy: function () {
-                            console.log("destroy backtrack!");
-                            // Tone.getContext().dispose();
-                            backtrack.current.dispose();
-                            Tone.getTransport().clear(updatePlayhead.current);
-
-                            this.externalPlayer = null;
-                            this.eventEmitter = null;
-                        },
-
-                        setSource: function (opts) {
-                            console.log("setSource backtrack!");
-                            if (this.isPlaying()) {
-                                this.pause();
-                            }
-
-                            // Update the Tone.js Player object with the new AudioBuffer
-                            this.externalPlayer.buffer.set(opts.webAudio.audioBuffer);
-                            return Promise.resolve();
-                        },
-
-                        play: async function () {
-                            console.log("play backtrack!");
-                            togglePlayback(true)
-                            this.eventEmitter.emit('player.playing', this.getCurrentTime());
-                            return Promise.resolve();
-                        },
-
-                        pause: function () {
-                            console.log("pause backtrack!");
-                            // Tone.getTransport().pause();
-
-                            this.eventEmitter.emit('player.pause', this.getCurrentTime());
-                        },
-
-                        isPlaying: function () {
-                            return Tone.getTransport().state === "started";
-                        },
-
-                        seek: async function (time) {
-                            console.log("seek backtrack! ");
-                            const position = Tone.Time(time, "s").toBarsBeatsSixteenths();
-                            togglePlayback(true, position);
-
-                            this.eventEmitter.emit('player.seeked', this.getCurrentTime());
-                            this.eventEmitter.emit('player.timeupdate', this.getCurrentTime());
-                        },
-
-                        isSeeking: function () {
-                            return false;
-                        },
-
-                        getCurrentTime: function () {
-                            return Tone.getTransport().seconds;
-                        },
-
-                        getDuration: function () {
-                            return this.externalPlayer.buffer.duration;
-                        }
-                    };
-
-                    const options = {
-                        // zoomview: {
-                        //     container: document.getElementById('zoomview-container')
-                        // },
-                        overview: {
-                            container: document.getElementById('overview-container')
-                        },
-                        player: player,
-                        webAudio: {
-                            audioBuffer: audioBuffer,
-                            scale: 128,
-                            multiChannel: false
-                        },
-                        keyboard: true,
-                        showPlayheadTime: true,
-                        zoomLevels: [128, 256, 512, 1024, 2048, 4096]
-                    };
-
-                    Peaks.init(options, function (err, peaksInstance) {
-                        if (err) {
-                            console.error(err.message);
-                            return;
-                        }
-                        peaks.current = peaksInstance;
-                    });
-                }).catch((error) => {
-                    console.error("Backtrack error: ", error);
-                }
-                );
+                setBacktrack(arrayBuffer);
             }
             fileReader.readAsArrayBuffer(backtrackBlob)
         } else {
-            if (backtrack.current) {
-                peaks.current.destroy();
-                socket.emit("conductor-backtrack", null);
-            }
+            setBacktrack(null);
         }
-
     }, [fileUpload.acceptedFiles]);
 
     return (
@@ -188,8 +52,6 @@ export const BacktrackButton = ({ backtrack, socket, togglePlayback }) => {
                     </Input>
                 </InputGroup>
             </FileUpload.RootProvider>
-            {/* <div id="zoomview-container"></div> */}
-            <div id="overview-container"></div>
         </VStack>
     )
 }
--- apps/Conductor/src/components/Tempo/TempoSlider.tsx
+++ apps/Conductor/src/components/Tempo/TempoSlider.tsx
@@ -1,34 +1,14 @@
 import { VStack, Slider, Switch, Spacer, } from "@chakra-ui/react"
-import React, { useState, useEffect } from "react"
-import { Socket } from 'socket.io-client';
-import * as Tone from "tone";
+import React, { useState, RefObject } from "react"
 
-import { DeviceType, TempoMode } from "../../types";
-import { convertTime } from "../../util";
+export const TempoSlider = ({tempo, setTempo }: { tempo: RefObject, setTempo: (t: number) => void }) => {
 
-export const TempoSlider = ({ socket, serverOffset }: { socket: Socket; serverOffset: any }) => {
-
-    const [tempo, setTempo] = useState<number>(60);
     const [isFluidMode, setIsFluidMode] = useState(false);
-
-    useEffect(() => {
-        console.log("Change Tempo!");
-        if (socket) {
-            const targetTime = convertTime("Server", Tone.immediate(), serverOffset)
-            console.log("Target Time: ", targetTime);
-            const position = "0:0:0";
-            socket.emit("conductor-change-tempo", targetTime, position, tempo, (newTargetTime: number) => {
-                console.log("New Target Time: ", newTargetTime);
-                let time2 = convertTime("Client", newTargetTime, serverOffset);
-                Tone.getTransport().bpm.setValueAtTime(tempo, time2);
-            });
-        }
-    }, [tempo]);
 
     return (
         <VStack>
             <Slider.Root minW={250} colorPalette={"blue"} min={40} max={200} defaultValue={[60]} onValueChange={(e) => isFluidMode && setTempo(e.value)} onValueChangeEnd={(e) => !isFluidMode && setTempo(e.value)} >
-                <Slider.Label>Tempo: {tempo}</Slider.Label>
+                <Slider.Label>Tempo:</Slider.Label>
                 <Slider.Control>
                     <Slider.Track>
                         <Slider.Range />
