1a5ba44a1f2dc565a4d2234c05c95308b1ad7f97
Garrett Eckl <geckl@mcneese.edu>
2025-08-05 02:47:35 -0500

    Moving setBacktrack to main App.tsx and adding listeners for Tonejs state

--- apps/Conductor/src/components/Backtrack/BacktrackButton.tsx
+++ apps/Conductor/src/components/Backtrack/BacktrackButton.tsx
@@ -1,84 +1,82 @@
-import { React, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { CloseButton, FileUpload, Input, InputGroup, useFileUpload, VStack } from "@chakra-ui/react"
 import { Socket } from 'socket.io-client';
-import { setBacktrack } from './BacktrackPlayer';
-import { timer } from '../../util';
 
-export const BacktrackButton = ({socket, togglePlayback, setIsBacktrack} : {socket: Socket, togglePlayback: (play: boolean, position?: string) => void, setIsBacktrack: (isBacktrack: boolean) => void}) => {
-    const fileUpload = useFileUpload({
-        maxFiles: 1,
-        accept: ["audio/mpeg", "audio/wav"],
-    });
+export const BacktrackButton = ({ setBacktrack, socket }: { setBacktrack: (ab: ArrayBuffer | null, s: Socket | null) => void, socket: Socket | null }) => {
+  const fileUpload = useFileUpload({
+    maxFiles: 1,
+    accept: ["audio/mpeg", "audio/wav"],
+  });
 
-      function readBacktrackFile(audioFile: File | null) {
-        if (socket) {
-          if (audioFile === null) {
-            socket.emit("conductor-backtrack", null);
-            console.log("Backtrack cleared");
-          } else {
-            const stream = audioFile.stream()
-            const reader = stream.getReader();
-            const readChunk = () => {
-              reader.read().then(({ done, value }) => {
-                if (done) {
-                  console.log("Stream finished");
-                  return;
-                }
-                socket.emit("conductor-backtrack", value);
-                // Continue reading the next chunk
-                readChunk();
-              });
-            };
-            // Start reading the stream
-            readChunk();
-          }
-        }
-      }
-
-    useEffect(() => {
-        const file = fileUpload.acceptedFiles[0];
-        if (file) {
-            console.log("New Backing Track Selected: ", file);
-            readBacktrackFile(file);
-            const backtrackBlob = file as Blob;
-            const fileReader = new FileReader();
-            fileReader.onloadend = () => {
-                const arrayBuffer = fileReader.result as ArrayBuffer
-                setBacktrack(arrayBuffer, setIsBacktrack, socket, togglePlayback);
+  function readBacktrackFile(audioFile: File | null) {
+    if (socket) {
+      if (audioFile === null) {
+        socket.emit("conductor-backtrack", null);
+        console.log("Backtrack cleared");
+      } else {
+        const stream = audioFile.stream()
+        const reader = stream.getReader();
+        const readChunk = () => {
+          reader.read().then(({ done, value }) => {
+            if (done) {
+              console.log("Stream finished");
+              return;
             }
-            fileReader.readAsArrayBuffer(backtrackBlob);
-        } else {
-            setBacktrack(null, setIsBacktrack, socket);
-        }
-    }, [fileUpload.acceptedFiles]);
+            socket.emit("conductor-backtrack", value);
+            // Continue reading the next chunk
+            readChunk();
+          });
+        };
+        // Start reading the stream
+        readChunk();
+      }
+    }
+  }
 
-    return (
-        <VStack>
-            <FileUpload.RootProvider gap="1" maxWidth="200px" colorPalette="blue" value={fileUpload}>
-                <FileUpload.HiddenInput />
-                <FileUpload.Label>Upload Backing Track:</FileUpload.Label>
-                <InputGroup
-                    bg="white"
-                    endElement={
-                        <FileUpload.ClearTrigger asChild>
-                            <CloseButton
-                                me="-1"
-                                size="xs"
-                                variant="plain"
-                                focusVisibleRing="inside"
-                                focusRingWidth="2px"
-                                pointerEvents="auto"
-                            />
-                        </FileUpload.ClearTrigger>
-                    }
-                >
-                    <Input asChild bg="brand.500">
-                        <FileUpload.Trigger>
-                            <FileUpload.FileText lineClamp={1} />
-                        </FileUpload.Trigger>
-                    </Input>
-                </InputGroup>
-            </FileUpload.RootProvider>
-        </VStack>
-    )
+  useEffect(() => {
+    const file = fileUpload.acceptedFiles[0];
+    if (file) {
+      console.log("New Backing Track Selected: ", file);
+      readBacktrackFile(file);
+      const backtrackBlob = file as Blob;
+      const fileReader = new FileReader();
+      fileReader.onloadend = () => {
+        const arrayBuffer = fileReader.result as ArrayBuffer
+        setBacktrack(arrayBuffer, socket);
+      }
+      fileReader.readAsArrayBuffer(backtrackBlob);
+    } else {
+      setBacktrack(null, socket);
+    }
+  }, [fileUpload.acceptedFiles]);
+
+  return (
+    <VStack>
+      <FileUpload.RootProvider gap="1" maxWidth="200px" colorPalette="blue" value={fileUpload}>
+        <FileUpload.HiddenInput />
+        <FileUpload.Label>Upload Backing Track:</FileUpload.Label>
+        <InputGroup
+          bg="white"
+          endElement={
+            <FileUpload.ClearTrigger asChild>
+              <CloseButton
+                me="-1"
+                size="xs"
+                variant="plain"
+                focusVisibleRing="inside"
+                focusRingWidth="2px"
+                pointerEvents="auto"
+              />
+            </FileUpload.ClearTrigger>
+          }
+        >
+          <Input asChild bg="brand.500">
+            <FileUpload.Trigger>
+              <FileUpload.FileText lineClamp={1} />
+            </FileUpload.Trigger>
+          </Input>
+        </InputGroup>
+      </FileUpload.RootProvider>
+    </VStack>
+  )
 }
